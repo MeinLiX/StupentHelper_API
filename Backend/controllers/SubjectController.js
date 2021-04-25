46224a166daa2f4d749036c0f4c214341751c62b
meinlix <meinlix@ukr.net>
2021-04-25 14:10:25 +0300

    UPDATE: Added Get request;

--- Backend/controllers/SubjectController.js
+++ Backend/controllers/SubjectController.js
@@ -3,7 +3,26 @@ import { models } from '../config/dbConnect.js';
 const Subject = models.subject;
 
 export async function FindUK(req, res) {
-    
+    try {
+        let Subjects = await Subject.findAll({
+            where: {
+                userId: req.user.idUser
+            }
+        });
+        if (Subjects) {
+            res.status(200).send({
+                success: true,
+                data: Subjects
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            success: false,
+            error: {
+                message: err.message
+            }
+        });
+    }
 }
 
 export async function Create(req, res) {
