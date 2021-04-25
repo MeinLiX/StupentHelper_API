159dbbceeeaf47bbae1c4bafd7cccd6eec19ea14
meinlix <meinlix@ukr.net>
2021-04-25 14:13:05 +0300

    UPDATE: Added Subject routes;

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,8 +2,10 @@ import {Router} from "express";
 import {checkAuthenticated} from "../config/passport.js";
 
 import {Pong, CheckAuthenticated} from "../controllers/RootController.js"
+
 import authorizationRoutes from "./authorization.routes.js";
 import usersRoutes from "./users.routes.js";
+import subjectsRoutes from "./subjects.routes.js";
 
 const router = Router();
 
@@ -11,8 +13,10 @@ router.get('/ping', Pong);
 router.get('/isAuthenticated', CheckAuthenticated);
 
 router.use('/', authorizationRoutes);
+
 router.use(checkAuthenticated);
 
 router.use('/users', usersRoutes);
+router.use('/subjects', subjectsRoutes);
 
 export default router;
