9c8e92c37ee141d257466574599856bcc7a018e7
meinlix <meinlix@ukr.net>
2021-04-26 23:11:29 +0300

    UPDATE: added GET request;

--- Backend/controllers/TeacherController.js
+++ Backend/controllers/TeacherController.js
@@ -1,9 +1,29 @@
-import {models} from '../config/dbConnect.js';
+import { models } from '../config/dbConnect.js';
+import { TException, TNotFoundModel } from '../utils/templatesRes.js'
 
 const Teacher = models.teacher;
 
 export async function FindUK(req,res){
-
+    try {
+        const Teachers = await Teacher.findAll({
+            where: {
+                userId: req.user.idUser
+            },
+            order: [
+                ['surname', 'ASC']
+            ]
+        });
+        if (Teachers) {
+            res.status(200).json({
+                success: true,
+                data: Teachers
+            });
+        } else {
+            TNotFoundModel(req, res, "Teacher");
+        }
+    } catch (err) {
+        TException(req, res, err);
+    };
 }
 
 export async function Create(req,res){
