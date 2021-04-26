b551c8f172b2ba3e01927f3e4749df2ef9c5443b
meinlix <meinlix@ukr.net>
2021-04-27 00:20:09 +0300

    UPDATE: Added CREATE teacher request;

--- Backend/controllers/TeacherController.js
+++ Backend/controllers/TeacherController.js
@@ -1,9 +1,10 @@
+import {v4 as uuid} from "uuid";
 import { models } from '../config/dbConnect.js';
 import { TException, TNotFoundModel } from '../utils/templatesRes.js'
 
 const Teacher = models.teacher;
 
-export async function FindUK(req,res){
+export async function FindUK(req, res) {
     try {
         const Teachers = await Teacher.findAll({
             where: {
@@ -26,15 +27,51 @@ export async function FindUK(req,res){
     };
 }
 
-export async function Create(req,res){
+export async function Create(req, res) {
+    let {surname,name,middle_name} = req.body;
+    
+    if (!surname || surname?.trim() == "") {
+        res.status(200).json({
+            success: false,
+            error: {
+                message: "The surname field can not be empty."
+            }
+        });
+        return;
+    };
+    try {
+        const CreateTeacher = await Teacher.create(
+            {
+                idTeacher: uuid(),
+                surname: surname.trim(),
+                name: name?.trim(),
+                'middle name': middle_name?.trim(),
+                userId: req.user.idUser
+            });
+        if (CreateTeacher) {
+            res.status(200).json({
+                success: true,
+                message: "Teacher is created.",
+                data: CreateTeacher
+            });
+        } else {
+            res.status(200).json({
+                success: false,
+                error: {
+                    message: "Trouble with DB.."
+                }
+            });
+        };
+    } catch (err) {
+        TException(req, res, err);
+    };
+}
+
+export async function Update(req, res) {
 
 }
 
-export async function Update(req,res){
-
-}
-
-export async function Delete(req,res){
+export async function Delete(req, res) {
     try {
         const DeletedTeacher = Teacher.destroy({
             where: {
