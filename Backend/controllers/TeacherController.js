9bb1c7d7003965ef0c2471de2a0f1d4c017ab57b
meinlix <meinlix@ukr.net>
2021-04-26 23:04:12 +0300

    NEW: initial teacher controller

---
+++ Backend/controllers/TeacherController.js
@@ -0,0 +1,19 @@
+import {models} from '../config/dbConnect.js';
+
+const Teacher = models.teacher;
+
+export async function FindUK(req,res){
+
+}
+
+export async function Create(req,res){
+
+}
+
+export async function Update(req,res){
+
+}
+
+export async function Delete(req,res){
+
+}
