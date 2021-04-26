df61108951c8802cabcf24e16ab1bc05a83b89fb
meinlix <meinlix@ukr.net>
2021-04-26 13:42:04 +0300

    UPDATE: Added UPDATE request; Use templates;

--- Backend/controllers/SubjectController.js
+++ Backend/controllers/SubjectController.js
@@ -1,33 +1,31 @@
 import { v4 as uuid } from "uuid";
 import { models } from '../config/dbConnect.js';
+import {TException, TNotFoundModel} from '../utils/templatesRes.js'
 
 const Subject = models.subject;
 
 export async function FindUK(req, res) {
     try {
-        let Subjects = await Subject.findAll({
+        const Subjects = await Subject.findAll({
             where: {
                 userId: req.user.idUser
             }
         });
         if (Subjects) {
-            res.status(200).send({
+            res.status(200).json({
                 success: true,
                 data: Subjects
             });
+        } else {
+            TNotFoundModel(req, res, "Subject");
         }
     } catch (err) {
-        res.status(500).send({
-            success: false,
-            error: {
-                message: err.message
-            }
-        });
+        TException(req, res, err);
     };
 }
 
 export async function Create(req, res) {
-    if (!req.body.name) {
+    if (!req.body.name || req.body.name == "") {
         res.status(200).json({
             success: false,
             error: {
@@ -37,11 +35,24 @@ export async function Create(req, res) {
         return;
     };
     try {
-        let CreateSubject = await Subject.create({
-            idSubject: uuid(),
-            name: req.body.name,
-            userId: req.user.idUser
-        });
+        const FoundSubject = await Subject.findOne({ where: { name: req.body.name } });
+        if (FoundSubject) {
+            res.status(200).json({
+                success: false,
+                error: {
+                    message: "This name is already taken."
+                }
+            });
+            return;
+        }
+
+        const CreateSubject = await Subject.create(
+            {
+                idSubject: uuid(),
+                name: req.body.name,
+                userId: req.user.idUser
+            });
+
         if (CreateSubject) {
             res.status(200).json({
                 success: true,
@@ -52,49 +63,69 @@ export async function Create(req, res) {
             res.status(200).json({
                 success: false,
                 error: {
-                    message: "This name already taken"
+                    message: "Trouble with DB.."
                 }
             });
         };
     } catch (err) {
-        res.status(500).json({
-            success: false,
-            error: {
-                message: err.message
-            }
-        });
+        TException(req, res, err);
     };
 }
 
 export async function Update(req, res) {
-
-}
-
-export async function Delete(req, res) {
     try {
-        let test = Subject.destroy({
+        const FoundSubject = await Subject.findOne({
             where: {
                 idSubject: req.params.idSubject,
                 userId: req.user.idUser
             }
         });
-        if (await test > 0) {
+        const UpdateSubject = await Subject.update(
+            {
+                name: req.body.name
+            },
+            {
+                where: {
+                    idSubject: req.params.idSubject,
+                    userId: req.user.idUser
+                }
+            }
+        );
+        if (UpdateSubject > 0) {
+            res.status(200).json({
+                success: true,
+                message: "Subject updated."
+            });
+        } else if (FoundSubject) {
+            res.status(200).json({
+                success: true,
+                message: "Subject not changed."
+            });
+        } else {
+            TNotFoundModel(req, res, "Subject");
+        };
+    } catch (err) {
+        TException(req, res, err);
+    };
+}
+
+export async function Delete(req, res) {
+    try {
+        const DeletedSubject = Subject.destroy({
+            where: {
+                idSubject: req.params.idSubject,
+                userId: req.user.idUser
+            }
+        });
+        if (await DeletedSubject > 0) {
             res.status(200).json({
                 success: true,
                 message: "Subject deleted."
             });
         } else {
-            res.status(200).json({
-                success: false,
-                message: "Subject not found."
-            });
+            TNotFoundModel(req, res, "Subject");
         };
     } catch (err) {
-        res.status(500).json({
-            success: false,
-            error: {
-                message: err.message
-            }
-        });
+        TException(req, res, err);
     };
 }
