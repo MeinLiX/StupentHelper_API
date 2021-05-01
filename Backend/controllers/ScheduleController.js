b039733017973be876e1a96332c48736ac1c10fb
meinlix <meinlix@ukr.net>
2021-05-02 01:33:01 +0300

    UPDATE: Adding DELETE schedule request, post for test;

--- Backend/controllers/ScheduleController.js
+++ Backend/controllers/ScheduleController.js
@@ -1,15 +1,105 @@
+import { v4 as uuid } from "uuid";
 import { models } from '../config/dbConnect.js';
+import { TException, TNotFoundModel, TNotNullAndEmpty } from '../utils/templatesRes.js'
 
-const Shedule = models.schedule;
+const Schedule = models.schedule;
+const Class = models.Class;
 
 export async function FindUK(req, res) {
-   
+    try {
+        const Schedules = await Schedule.findAll({
+            where: {
+                userId: req.user.idUser
+            }
+        });
+        if (Schedules) {
+         
+
+
+        } else {
+            TNotFoundModel(req, res, "Schedule");
+        }
+    } catch (err) {
+        TException(req, res, err);
+    };
+
 }
 
 export async function Create(req, res) {
-    
+    let { $class, subjectId, weekdayId, classtypeId, teacherId, parity } = req.body;
+    console.log(req.body);
+    parity = parity ?? null;
+    try {
+        //validation id's here:
+
+
+
+        //Try creation:
+        const CreateClass = await Class.create(
+            {
+                idClass: uuid(),
+                number: $class?.number,
+                from: new Date(0, 0, 0, $class?.from?.split(':')[0], $class?.from?.split(':')[1]),
+                to: new Date(0, 0, 0, $class?.to?.split(':')[0], $class?.to?.split(':')[1])
+            });
+        if (!CreateClass) {
+            res.status(200).json({
+                success: false,
+                error: {
+                    message: "Trouble with DB.."
+                }
+            });
+            return;
+        }
+        const CreateSchedule = await Schedule.create(
+            {
+                idSchedule: uuid(),
+                parity: parity,
+                classId: CreateClass.idClass,
+                subjectId: subjectId,
+                weekdayId: weekdayId,
+                classtypeId: classtypeId,
+                teacherId: teacherId,
+                userId: req.user.idUser
+            });
+        console.log(CreateSchedule, CreateClass);
+        if (CreateSchedule) {
+            res.status(200).json({
+                success: true,
+                message: "Schedule is created.",
+                data: CreateSchedule
+            });
+        } else {
+            res.status(200).json({
+                success: false,
+                error: {
+                    message: "Trouble with DB.."
+                }
+            });
+        };
+
+    } catch (err) {
+        TException(req, res, err);
+    };
 }
 
 export async function Delete(req, res) {
-   
+    try {
+        const DeletedSchedule = Schedule.destroy({
+            where: {
+                idSchedule: req.params.idSchedule,
+                userId: req.user.idUser
+            }
+        });
+        if (await DeletedSchedule > 0) {
+            res.status(200).json({
+                success: true,
+                message: "Schedule deleted."
+            });
+        } else {
+            TNotFoundModel(req, res, "Schedule");
+        };
+    } catch (err) {
+        TException(req, res, err);
+    };
 }
