4d36cb7952ca69ca8cd22c0c3eb28cf745057dfb
meinlix <meinlix@ukr.net>
2021-05-01 23:42:22 +0300

    NEW: init schedule controller;

---
+++ Backend/controllers/ScheduleController.js
@@ -0,0 +1,15 @@
+import { models } from '../config/dbConnect.js';
+
+const Shedule = models.schedule;
+
+export async function FindUK(req, res) {
+   
+}
+
+export async function Create(req, res) {
+    
+}
+
+export async function Delete(req, res) {
+   
+}
